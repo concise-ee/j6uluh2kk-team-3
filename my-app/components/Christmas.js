298b4360695ead6dce0570c8e9c1b2b413163e4d
Kaur Kaelep <kaur.kaelep@concise.ee>
2021-12-07 14:27:28 +0200

    haha

--- my-app/components/Christmas.js
+++ my-app/components/Christmas.js
@@ -3,12 +3,8 @@ import React from 'react'
 export const Christmas = ({christmas}) => {
     return(
         <div className="container">
-<<<<<<< HEAD
-            <h2>Is it christmas yet?? {christmas.isItChristmasYet ? 'Yep' : 'Nope'}</h2>
-=======
             {/* <h2>Is it christmas yet? {christmas.isItChristmasYet ? 'Yep' : 'Nope'}</h2> */}
             <img src="https://s.yimg.com/os/creatr-uploaded-images/2021-02/572c4830-721d-11eb-bb63-96959c3b62f2" width="100%" />
->>>>>>> f6ec27e (haha)
         </div>
     )
 }
