72015508e06fff2fb5d84bf1952fc3a1d0f0aab1
Kaur Kaelep <kaur.kaelep@concise.ee>
2021-12-07 15:32:18 +0200

    kaur

--- my-app/components/Kaur.js
+++ my-app/components/Kaur.js
@@ -2,25 +2,17 @@ import React, { useState, useEffect } from 'react';
 
 const Kaur = () => {
     return (
-        <div style={{
-            width: '100%',
-            height: '100%',
-            backgroundColor: 'red',
-            display: 'flex',
-            flexWrap: 'wrap',
-            alignContent: 'start',
-            overflow: 'hidden'
-        }}>
+        <div style={wrapperStyle}>
+            <div style={largeTreeWrapper}>
+                <Tree treeSize={100} treeOffset={70} />
+                <Concise />
+            </div>
             {Array.from({ length: 1500 }, (el, idx) => <Tree key={idx} />)}
         </div>
     )
 }
 
-const treeSize = 16;
-const treeOffset = 10;
-const treeSpacing = 5;
-
-const Tree = () => {
+const Tree = ({ treeSize = 16, treeOffset = 10, treeSpacing = 5 }) => {
     const [color, setColor] = useState(CSS_COLOR_NAMES[Math.floor(Math.random() * CSS_COLOR_NAMES.length)]);
 
     const getRandColor = () => CSS_COLOR_NAMES[Math.floor(Math.random() * CSS_COLOR_NAMES.length)];
@@ -28,26 +20,66 @@ const Tree = () => {
     useEffect(() => {
         const interval = setInterval(() => setColor(getRandColor()), 2000);
         return () => clearInterval(interval);
-    });
+    }, []);
 
     return (
         <div style={{ position: 'relative', width: (treeSize * 2) + treeSpacing, height: treeSize * 3 }}>
             {Array.from({ length: 3 }).map((el, idx) => {
                 return (
                     <div key={idx} style={{
-                        width: 0,
-                        height: 0,
-                        position: 'absolute',
+                        ...treeStyle,
                         top: (treeSize - treeOffset * idx),
                         borderLeft: `${treeSize}px solid transparent`,
                         borderRight: `${treeSize}px solid transparent`,
                         borderBottom: `${treeSize}px solid ${color === 'Red' ? getRandColor() : color}`,
-                    }} /> 
+                    }} />
                 )
             })}
         </div>
     )
 };
+
+const concise = ["c", "o", "n", "c", "i", "s", "e"]
+
+const Concise = () => {
+    const [letters, setLetters] = useState(concise);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setLetters(p => p.map(x => [Math.random(), x]).sort(([a], [b]) => a - b).map(([_, x]) => x))
+        }, 1000);
+        return () => clearInterval(interval);
+    }, []);
+
+    return (
+        <p style={{
+            color: '#ff0',
+            fontSize: 60,
+            textShadow: '0 0 2px #10161e'
+        }}>{letters}</p>
+    )
+}
+
+const wrapperStyle = {
+    width: '100%',
+    height: '100%',
+    backgroundColor: 'red',
+    display: 'flex',
+    flexWrap: 'wrap',
+    alignContent: 'start',
+    overflow: 'hidden',
+    position: 'relative',
+};
+
+const treeStyle = {
+    width: 0,
+    height: 0,
+    position: 'absolute',
+    pointerEvents: 'none',
+    userSelect: 'none'
+};
+
+const largeTreeWrapper = { position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', zIndex: 1 }
 
 const CSS_COLOR_NAMES = [
     "AliceBlue",
