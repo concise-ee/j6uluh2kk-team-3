0683ace3d4cd916677f5b3d59674280d72e21b85
Kurmo Rootsi <kurmo.rootsi@easypark.net>
2021-12-07 16:30:07 +0200

    background image

--- my-app/components/Kurmo.js
+++ my-app/components/Kurmo.js
@@ -4,12 +4,15 @@ const Kurmo = () => {
     return (
       <div class="kurmo">
         <div class="santa-container" style={{
+          backgroundImage: "url(" + "https://gcdn.pbrd.co/images/dfIb1pPCtZtc.jpg" + ")",
              width: '100vw',
              height: '100vh',
              display: 'flex',
             flexDirection: 'column',
             justifyContent: 'center',
             alignItems: 'center',
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
          }}>
          <div class="santa-hat" style={{
            backgroundImage: "url(" + "https://i.ibb.co/pJ9ZbrW/hat.png" + ")",
