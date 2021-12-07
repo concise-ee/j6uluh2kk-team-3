21b77baab9df3f482fed7ab19f9ff593452e5da2
Rene Rakk <rene.rakk@easypark.net>
2021-12-07 14:58:52 +0200

    home alone

--- my-app/components/Rene.js
+++ my-app/components/Rene.js
@@ -3,7 +3,10 @@ import React from 'react'
 const Rene = () => {
     return (
         <div>
-            
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/rgEP1niScLc?controls=0&amp&autoplay=1;start=1"
+                    title="YouTube video player" frameBorder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowFullScreen></iframe>
         </div>
     )
 }
