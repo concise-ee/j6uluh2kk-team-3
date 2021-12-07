89835f2bd75a893f1eab6a9a91530e4814914aab
Kaur Kaelep <kaur.kaelep@concise.ee>
2021-12-07 16:08:12 +0200

    a

--- my-app/components/Allan.js
+++ my-app/components/Allan.js
@@ -2,8 +2,9 @@ import React from 'react'
 
 const Allan = () => {
     return (
-        <div>
-            <img src="https://scontent.ftll2-1.fna.fbcdn.net/v/t1.6435-9/41905284_1835010113283143_4543487323655897088_n.jpg?_nc_cat=102&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=udtch3n8p68AX8auZDt&_nc_ht=scontent.ftll2-1.fna&oh=3f710c9608435269f978420445c25854&oe=61D587DC"/>
+        <div style={{width: '100%'}}>
+            <img src="https://scontent.ftll2-1.fna.fbcdn.net/v/t1.6435-9/41905284_1835010113283143_4543487323655897088_n.jpg?_nc_cat=102&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=udtch3n8p68AX8auZDt&_nc_ht=scontent.ftll2-1.fna&oh=3f710c9608435269f978420445c25854&oe=61D587DC" style={{ width: '100%', height: '100vh', objectFit: 'cover' }}/>
+            <p style={{ color: '#fff', textShadow: '0 0 2px #000', position: 'absolute', top: 0, fontSize: 38, whiteSpace: 'nowrap', left: '50%', transform: 'translateX(-50%)', fontWeight: 'bold'}}>FALLEN SOLDIER</p>
         </div>
     )
 }
