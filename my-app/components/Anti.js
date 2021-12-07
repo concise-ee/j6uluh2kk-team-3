fb1dd70480872b4f9e5a8ed6eb70e0c1a93f9bc3
Anti Orgla <anti.orgla@easypark.net>
2021-12-07 15:28:14 +0200

    Anti solution

--- my-app/components/Anti.js
+++ my-app/components/Anti.js
@@ -1,10 +1,11 @@
-import React from 'react'
+import React from 'react';
 
 const Anti = () => {
+    
     const submitNumber = async (event) => {
-        let randIdx = event.target.num.value
-        while (randIdx ===event.target.num.value)  {
-            randIdx = Math.floor(Math.random() * 10)+1
+        let randIdx = event.target.num.value;
+        while (randIdx === event.target.num.value) {
+            randIdx = Math.floor(Math.random() * 10) + 1;
         }
         
         event.preventDefault();
@@ -12,19 +13,43 @@ const Anti = () => {
     };
     
     return (
-        <div>
-            <p>WIN 1000€ IN CASH NOW!!</p>
-            
-            <p>Guess random number from 1-10 to win:</p>
-    
-            <form className='flex flex-col' onSubmit={submitNumber}>
-                <input className='mb-4 border-b-2' id='num' type='text' required />
-                <br/>
-                <button type='submit' className='px-4 py-2 font-bold text-white bg-blue-500 rounded-full hover:bg-blue-700'>GUESS</button>
-            </form>
+        <div style={{
+            textAlign: 'center',
+            height: '100%',
+            backgroundColor: 'pink',
+        }}>
+            <div style={{
+                textAlign: 'center',
+                backgroundColor: 'pink',
+                padding: 25,
+                color: 'red',
+                animation: 'blinker 0.1s cubic-bezier(.5, 0, 1, 1) infinite alternate',
+                fontSize: 'xxx-large'
+            }}><strong>WIN 1000€ IN CASH NOW!!</strong></div>
+            <div style={{
+                padding: 10
+            }}>Transfer 1€ to <strong>EE1220384351625147367298</strong> and try your luck!</div>
+            <div style={{
+                padding: 10
+            }}>Guess random number from 1-10 to win:</div>
+            <div>
+                <form className="flex flex-col" onSubmit={submitNumber}>
+                    
+                    <input className="mb-2 border-b-2" id="num" type="number" step="any" min="1" max="10" required
+                           style={{
+                               textAlign: 'center',
+                               fontSize: 'xxx-large',
+                               width: 100
+                           }}/>
+                    <br/>
+                    <button type="submit"
+                            className="px-4 py-2 font-bold text-black bg-blue-100 rounded-full hover:bg-blue-700">GUESS
+                    </button>
+                </form>
+            </div>
         </div>
-        
-    )
-}
+    
+    );
+};
 
-export default Anti
+export default Anti;
